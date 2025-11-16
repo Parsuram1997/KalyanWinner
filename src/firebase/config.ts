b1ab0e8620ef3ea3a3cc04024e574818f38e3cb3
PARSURAM NAIK <parsuramnaik5@gmail.com>
2025-11-16 09:19:38 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7786701397-58781",
+  "appId": "1:54126435357:web:008c3543e5ec3a1ab27af4",
+  "apiKey": "AIzaSyDHQc1df1xRk8hHZgqTzq-X173aMBoSe5E",
+  "authDomain": "studio-7786701397-58781.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "54126435357"
+};
