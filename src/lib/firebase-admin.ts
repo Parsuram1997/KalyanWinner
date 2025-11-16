7e3934499342aaf95c916b5a5af368876956fe6f
PARSURAM NAIK <parsuramnaik5@gmail.com>
2025-11-16 18:48:06 +0000

    abhi follwingb w error aya.

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,4 +1,4 @@
-'use server';
+
 import { initializeApp, getApps, getApp, App } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 import { getFirestore } from 'firebase-admin/firestore';
