a1d223c9d9a520f86be6b246e45b78bee70be561
PARSURAM NAIK <parsuramnaik5@gmail.com>
2025-11-16 20:10:32 +0000

    Button me atleast half inch ka padding rakho

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -20,11 +20,11 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-10 px-4 py-2",
-        sm: "h-9 rounded-md px-3",
-        lg: "h-11 rounded-md px-8",
+        default: "h-auto px-6 py-4",
+        sm: "h-auto rounded-md px-4 py-2",
+        lg: "h-auto rounded-md px-10 py-5",
         icon: "h-10 w-10",
-        xs: "h-8 rounded-sm px-2 text-xs",
+        xs: "h-auto rounded-sm px-3 py-1 text-xs",
       },
     },
     defaultVariants: {
