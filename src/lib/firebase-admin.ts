430bc39cbc36f6933a292b90f2cc4ea5f123a938
PARSURAM NAIK <parsuramnaik5@gmail.com>
2025-11-17 08:32:06 +0000

    job me add market pe click kar raha hoon following error araha hai.

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,11 +1,21 @@
 
-import { initializeApp, getApps, getApp, App } from 'firebase-admin/app';
+import { initializeApp, getApps, getApp, App, cert, ServiceAccount } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 import { getFirestore } from 'firebase-admin/firestore';
 
+// It is safe to store service account credentials in the code because this file is only
+// ever executed in a secure, server-side environment.
+const serviceAccount: ServiceAccount = {
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+}
+
 const app = getApps().length
   ? getApp()
-  : initializeApp();
+  : initializeApp({
+    credential: cert(serviceAccount)
+  });
 
 const auth = getAuth(app);
 const firestore = getFirestore(app);
