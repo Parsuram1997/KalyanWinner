428b2e32deaee32f49f83454194c51d89d7f19a2
PARSURAM NAIK <parsuramnaik5@gmail.com>
2025-11-29 04:30:57 +0000

    ok

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -3,6 +3,9 @@
 import { create } from 'zustand';
 import { type ToastActionElement, type ToastProps } from '@/components/ui/toast';
 
+const TOAST_LIMIT = 5;
+const TOAST_REMOVE_DELAY = 1000000;
+
 type ToasterToast = Omit<ToastProps, 'id'> & {
   id: string;
   title?: React.ReactNode;
@@ -26,10 +29,10 @@ const useToastStore = create<ToastState>((set) => ({
   toasts: [],
   add: (props) => {
     const id = genId();
-    const newToast = { ...props, id };
+    const newToast = { ...props, id, duration: props.duration || 3000 };
 
     set((state) => ({
-      toasts: [newToast, ...state.toasts],
+      toasts: [newToast, ...state.toasts].slice(0, TOAST_LIMIT),
     }));
   },
   dismiss: (toastId) => {
