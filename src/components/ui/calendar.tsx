52fcc209023455fd29adafe7e1456f0b911d59aa
PARSURAM NAIK <parsuramnaik5@gmail.com>
2025-12-17 04:39:39 +0000

    coloum aur row bhi dikh na chaiye.

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -4,7 +4,7 @@
 import * as React from "react"
 import { ChevronLeft, ChevronRight } from "lucide-react"
 import { DayPicker, DropdownProps } from "react-day-picker"
-import { format, getMonth, getYear } from "date-fns"
+import { format } from "date-fns"
 
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
@@ -37,17 +37,17 @@ function Calendar({
             months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
             month: "space-y-4",
             caption: "flex justify-between pt-1 relative items-center px-1",
-            caption_label: "text-sm font-medium hidden", // Hide default label, we use dropdowns
+            caption_label: "flex items-center text-sm font-medium",
             caption_dropdowns: "flex items-center gap-1",
             nav: "space-x-1 flex items-center",
             nav_button: cn(
               buttonVariants({ variant: "outline" }),
               "h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100"
             ),
-            nav_button_previous: "absolute left-1",
-            nav_button_next: "absolute right-1",
+            nav_button_previous: "",
+            nav_button_next: "",
             table: "w-full border-collapse space-y-1",
-            head_row: "flex justify-between",
+            head_row: "flex w-full justify-between",
             head_cell:
               "text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]",
             row: "flex w-full mt-2 justify-between",
