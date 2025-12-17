b218b97a43eff9a20e32fba64af08e8786f6f794
PARSURAM NAIK <parsuramnaik5@gmail.com>
2025-12-17 04:44:09 +0000

    abe yar bhai abhi bhi bigda hua hai.tu khud dekh

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,15 +1,12 @@
-
 "use client"
 
 import * as React from "react"
 import { ChevronLeft, ChevronRight } from "lucide-react"
-import { DayPicker, DropdownProps } from "react-day-picker"
+import { DayPicker } from "react-day-picker"
 import { format } from "date-fns"
 
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
-import { ScrollArea } from "./scroll-area"
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>
 
@@ -33,12 +30,12 @@ function Calendar({
       <div className="p-3">
         <DayPicker
           showOutsideDays={showOutsideDays}
+          className="p-0"
           classNames={{
             months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
             month: "space-y-4",
             caption: "flex justify-between pt-1 relative items-center px-1",
-            caption_label: "flex items-center text-sm font-medium",
-            caption_dropdowns: "flex items-center gap-1",
+            caption_label: "text-sm font-medium",
             nav: "space-x-1 flex items-center",
             nav_button: cn(
               buttonVariants({ variant: "outline" }),
@@ -47,7 +44,7 @@ function Calendar({
             nav_button_previous: "",
             nav_button_next: "",
             table: "w-full border-collapse space-y-1",
-            head_row: "flex w-full",
+            head_row: "flex w-full mt-2",
             head_cell:
               "text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]",
             row: "flex w-full mt-2",
@@ -71,40 +68,6 @@ function Calendar({
           components={{
             IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
             IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
-            Dropdown: ({ value, onChange, children, ...props }: DropdownProps) => {
-              const options = React.Children.toArray(children) as React.ReactElement<React.HTMLProps<HTMLOptionElement>>[];
-              const selected = options.find((child) => child.props.value === value);
-              const handleChange = (value: string) => {
-                const changeEvent = {
-                  target: { value },
-                } as React.ChangeEvent<HTMLSelectElement>;
-                onChange?.(changeEvent);
-              };
-              return (
-                <Select
-                  value={value?.toString()}
-                  onValueChange={(value) => {
-                    handleChange(value);
-                  }}
-                >
-                  <SelectTrigger className="pr-1.5 focus:ring-0 h-8 text-xs w-[6rem]">
-                    <SelectValue>{selected?.props?.children}</SelectValue>
-                  </SelectTrigger>
-                  <SelectContent position="popper">
-                    <ScrollArea className="h-48">
-                      {options.map((option, id: number) => (
-                        <SelectItem
-                          key={`${id}`}
-                          value={option.props.value?.toString() ?? ""}
-                        >
-                          {option.props.children}
-                        </SelectItem>
-                      ))}
-                    </ScrollArea>
-                  </SelectContent>
-                </Select>
-              );
-            },
           }}
           {...props}
         />
@@ -115,4 +78,3 @@ function Calendar({
 Calendar.displayName = "Calendar"
 
 export { Calendar }
-
